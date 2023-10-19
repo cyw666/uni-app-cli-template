ec8e94f6b95d3097084abba38542fc5fef9c4b96
cyw <cyw19931210@163.com>
2023-10-19 15:38:15 +0800

    fix:移除vite-plugin-unocss-to-uni库，导致开发环境编译异常

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import { fileURLToPath, URL } from 'node:url'
 import uni from '@dcloudio/vite-plugin-uni'
 import Unocss from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
-import { UnocssToUni } from 'vite-plugin-unocss-to-uni'
+// @ts-ignore
+import uniAxiosAdapter from '@uni-helper/axios-adapter/vite'
 
 // https://vitejs.dev/config/
 
@@ -16,9 +17,7 @@ export default defineConfig(({ command, mode, ssrBuild }) => {
   const commonPlugins = [
     uni(),
     Unocss(),
-
-    UnocssToUni(),
-
+    uniAxiosAdapter({}),
     AutoImport({
       imports: ['vue'],
       dts: 'src/auto-imports.d.ts',
@@ -44,6 +43,25 @@ export default defineConfig(({ command, mode, ssrBuild }) => {
         '#': fileURLToPath(new URL('./types', import.meta.url)),
       },
     },
+    // 配置前端服务地址和端口
+    server: {
+      host: '0.0.0.0',
+      port: 8081,
+      // 是否开启 https
+      https: false,
+      proxy: {
+        // 测试
+        // '/query': 'http://172.16.50.140:30477',
+        // 生产
+        '/query': 'https://digitalhuman.189.cn',
+        // '/query': 'http://10.135.139.53:30477',
+        // 本地
+        // '/query': 'http://192.168.137.1:8082',
+      },
+    },
+    // esbuild: {
+    //   drop: ['console', 'debugger'],
+    // },
     build: {
       minify: 'terser',
       terserOptions: {
